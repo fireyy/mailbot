a33ee97bec57a4c6f329b3716ca7de80bf417c2e
fireyy <fireyy@gmail.com>
2013-08-09 18:08:07 +0800

    add style

--- libs/pagination.js
+++ libs/pagination.js
@@ -20,31 +20,39 @@ var paginate = function(count, nPerPage, pageNumber, opts) {
   if (settings['prev_text'] && settings['current'] && 1 < settings['current']) {
     link = settings["base"].replace("%_%", settings["format"]);
     link = link.replace("%#%", settings["current"] - 1);
-    page_links.push('<li><a class="prev" href="' + link + settings["add_args"] + '">' + settings["prev_text"] + '</a></li>');
+    page_links.push('<a class="prev" href="' + link + settings["add_args"]
+     + '">' + settings["prev_text"] + '</a>');
   }
   for (n=1; n <= settings["total"]; n++) {
     n_display = n;
     if (n === settings["current"]) {
-      page_links.push('<li class="active"><b>' + n_display + '</b></li>');
+      page_links.push('<strong>' + n_display + '</strong>');
       dots = true;
     } else {
-      if (settings["show_all"] || (n <= settings["end_size"] || (settings["current"] && n >= settings["current"] - settings["mid_size"] && n <= settings["current"] + settings["mid_size"]) || n > settings["total"] - settings["end_size"])) {
+      if (settings["show_all"] || (n <= settings["end_size"]
+       || (settings["current"] && n >= settings["current"] - settings["mid_size"]
+       && n <= settings["current"] + settings["mid_size"])
+       || n > settings["total"] - settings["end_size"])) {
         link = settings["base"].replace("%_%", settings["format"]);
         link = link.replace("%#%", n);
-        page_links.push('<li><a href="' + link + settings["add_args"] + '">' + n_display + '</a></li>');
+        page_links.push('<a href="' + link + settings["add_args"] + '">' 
+                        + n_display + '</a>');
         dots = true;
       } else if (dots && !settings["show_all"]) {
-        page_links.push('<li class="disabled"><b>&#8230;</b></li>');
+        page_links.push('<strong>&#8230;</strong>');
         dots = false;
       }
     }
   }
-  if (settings["prev_next"] && settings["current"] && (settings["current"] < settings["total"] || -1 === settings["total"])) {
+  if (settings["prev_next"] && settings["current"] 
+      && (settings["current"] < settings["total"] 
+          || -1 === settings["total"])) {
     link = settings["base"].replace("%_%", settings["format"]);
     link = link.replace("%#%", parseInt(settings["current"]) + 1);
-    page_links.push('<li><a class="next" href="' + link + settings["add_args"] + '">' + settings["next_text"] + '</a></li>');
+    page_links.push('<a class="next" href="' + link + settings["add_args"]
+     + '">' + settings["next_text"] + '</a>');
   }
-  return '<div class="pagination"><ul>' + page_links.join("\n") + '</ul></div>';
+  return '<aside class="paging">' + page_links.join("\n") + '</aside>';
 }
 
 function parseAdditionalArgs(args){
